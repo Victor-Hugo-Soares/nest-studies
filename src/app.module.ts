dd564593a4e912039f8d095a89493c6f58f32451
victor_hugo <vsoareslins452@gmail.com>
2023-10-11 11:48:17 -0300

    autoSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { GraphQLModule} from '@nestjs/graphql'
+import { GraphQLModule} from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {} 
